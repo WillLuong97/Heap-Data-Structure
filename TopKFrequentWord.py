0aee28a08b3f2121bf7d280b2e3ae6d15fc86ab0
Will Luong <tluong@stedwards.edu>
2020-11-11 16:06:19 -0600

    Python3 implementation to find the element that are repeated K amount of time in the array using Heap implementation

--- TopKFrequentWord.py
+++ TopKFrequentWord.py
@@ -23,18 +23,66 @@ Follow up:
 Try to solve it in O(n log k) time and O(n) extra space.
 
 '''
-import collections
+
+from collections import Counter
+from collections import OrderedDict
+
+import heapq
 #Count the frequency of each word and then assign them into a list and sort from smallet to largest, this way we would be able to find the repeating elemnt
 def topKFrequent(words, k):
-    p = 
+    #base case: no word or k, return nothing
+    if not words or not k: 
+        return None
+
+    #variable to store the final result
+    result = []
+    # create a dictionary to store the amout of frequency an element exisit in: 
+    freqDict = list(Counter(words).items())
+
+    #Sort the array and then reverse it so that all the element with higher frequency can be accessed first 
+    sotredDict = OrderedDict(sorted(freqDict))
+
+    #reverse the dictioanry
+    reversedDict = sorted(sotredDict.items(), key=lambda x:x[1], reverse=True)
+    
+    for element, frequency in reversedDict:
+        result.append(element)
+
+    return result[0:k]
+#time complexity: O(n), the algorithm will have to go through every element in the array to count its frequency, not to mention, there are sorting operation, which is built in, so its time complexity would 
+#o(nlogn). 
+#space complexity: O(n), we create a dictionary from all the unique element in the array
 
 
+#Function to solve the problem using the Heap data structure to sovle the problem, hoping to solve it with a lesser time complexity
 
+def topKFrequent_HEAP(words, k):
+    #base case: 
+    if not words or not k: 
+        return None
+
+    #create a dictionarny to count the element frequency
+    freqDict = Counter(words).items()
+    #create the element frequency array
+    heap = [(-freq, element) for element, freq in freqDict]
+    #turn it into a heap
+    heapq.heapify(heap)
+    #keep popping the array until it reaches k
+    return [heapq.heappop(heap)[1] for _ in range(k)]
+
+    
+#time complexity: O(nlog(k))
+#space complexity: O(n)
 def main():
     print("TESTING TOP K FREQUENT WORDS...")
     arr1 = ["i", "love", "leetcode", "i", "love", "coding"]
     k = 2
+    arr2 = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]
+    k_2 = 4
     print(topKFrequent(arr1, k))
+    print(topKFrequent(arr2, k_2))
+    print(topKFrequent_HEAP(arr1, k))
+    print(topKFrequent_HEAP(arr2, k_2))
     print("END OF TESTING...")
 
 main()
